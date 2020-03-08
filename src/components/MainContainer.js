8f27143c8a6286de393b4ce22f054eeab185f18f
Brandon James <brandonj2858@gmail.com>
2020-03-08 10:47:23 -0400

    char select

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -9,7 +9,8 @@ class MainContainer extends React.Component {
     this.state = {
       characters: [],
       matches: [],
-      suggestions: []
+      suggestions: [],
+      selectedCharacter: [],
     }
 
   }
@@ -38,7 +39,17 @@ class MainContainer extends React.Component {
       const regex = new RegExp(input, 'gi');
       const suggestions = characterNames.filter((name) => (regex.test(name)))
       this.setState({suggestions: suggestions})
+      console.log(this.state.suggestions)
     }
+  }
+
+  handleClick(evt) {
+    console.log(evt.target.innerText);
+    const selectedChar = this.state.characters.filter((char) => {
+      return char.name === evt.target.innerText
+    })
+    this.setState({selectedCharacter: selectedChar});
+    console.log(this.state.selectedCharacter)
   }
 
 
@@ -57,8 +68,25 @@ class MainContainer extends React.Component {
         <div className="searchSuggestions">
         <input className="searchBox" type="text" onChange={this.handleChange} value={this.props.input}/>
 
-        {this.state.suggestions.map((char) => {return <li className="suggestionList"> {char}</li>})}
-      
+        {this.state.suggestions.map((char) => {return <li onClick={(evt) => this.handleClick(evt) } className="suggestionList"> {char}</li>})}
+
+        </div>
+
+        <div className="characterView">
+        {this.state.selectedCharacter.map((char) => {
+          return(
+          <div>
+          <li>Name: {char.name}</li>
+          <li>Height: {char.height}</li>
+          <li>Mass: {char.mass}</li>
+          <li>Hair: {char.hair_color}</li>
+          <li>Skin Color: {char.skin_color}</li>
+          <li>Eyes: {char.eye_color}</li>
+          <li>DOB: {char.birth_year}</li>
+          <li>Gender: {char.gender}</li>
+          </div>)
+        })}
+
         </div>
 
       </div>
